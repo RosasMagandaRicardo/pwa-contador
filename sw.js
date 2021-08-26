b82cf3b40076e4d07d65aa6591c0895ee5b32e31
Ricardo Rosas Maganda <36800151+RosasMagandaRicardo@users.noreply.github.com>
2021-08-26 14:13:00 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,62 @@
+const CACHE_ELEMENTS = [
+  "./",
+  "https://unpkg.com/react@17/umd/react.production.min.js",
+  "https://unpkg.com/react-dom@17/umd/react-dom.production.min.js",
+  "https://unpkg.com/@babel/standalone/babel.min.js",
+  "./style.css",
+  "./components/Contador.js",
+  "./register.js"
+];
+
+const CACHE_NAME = "v1_cache_contador_react";
+
+//INSTALL
+//PRIMER PASO PROCESO DE INSTALACION
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      cache
+        .addAll(CACHE_ELEMENTS)
+        .then(() => {
+          self.skipWaiting();
+        })
+        .catch(console.log);
+    })
+  );
+});
+
+//ACTIVATE
+//VALIDACIONES PARA EL CONTROL DE VERSIONES DEL CACHÉ
+self.addEventListener("activate", (e) => {
+  const cacheWhitelist = [CACHE_NAME];
+
+  e.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cacheName) => {
+            return (
+              cacheWhitelist.indexOf(cacheName) === -1 &&
+              caches.delete(cacheName)
+            );
+          })
+        );
+      })
+      .then(() => self.clients.claim())
+  );
+});
+
+//FETCH
+//VERIFICAR SI LAS RUTAS ESTÁN CACHEADAS, Y SI NO, CACHEARLAS
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      if (res) {
+        return res;
+      }
+
+      return fetch(e.request);
+    })
+  );
+});
